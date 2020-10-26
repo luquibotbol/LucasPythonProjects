cb34a5c9f9d1377515c6867f32593f61cbe26880
Lucas Botbol <luquibotbol@gmail.com>
2020-10-26 15:40:58 -0500

    Create prime.py
    This program finds all prime numbers that you want

---
+++ prime.py
@@ -0,0 +1,10 @@
+num = input("Until what number do you want to find prime? \n")
+for x in range(int(num)):
+    i = 0
+    for y in range(2, x):
+        if x % y == 0:
+            pass
+        else:
+            i = i + 1
+    if i == x - 2:
+        print(x)
